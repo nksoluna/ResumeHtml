f9b4fbc5f253b4e96f0294b68a64214d4cee10d9
nksoluna <xitnikz@gmail.com>
2021-06-05 01:04:31 +0700

    update header

--- javascript/activeonclick.js
+++ javascript/activeonclick.js
@@ -1,3 +1,7 @@
-$(document).on('click' , 'ul li' , function() {
+$(document).on('click' , 'nav-links a' , function() {
     $(this).addClass('active').siblings().removeClass('active')
+})
+
+$(document).on('click' , 'ul a' , function () {
+    $(this).addClass('skill-active').siblings().removeClass('skill-active')
 })
